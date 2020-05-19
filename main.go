6096e2eb965d4eaa024ba90fa196a6af73780745
wayanjimmy <jimmyeatcrab@gmail.com>
2020-05-19 12:58:34 +0700

    enable search query from flags
    enable search for private zettel notes

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"errors"
+	"flag"
 	"fmt"
-	"log"
+	"os"
 
 	"github.com/google/go-github/v31/github"
 	"golang.org/x/oauth2"
@@ -15,29 +17,61 @@ type Items struct {
 }
 
 type Item struct {
-	Arg string `json:"arg"`
+	Arg      string `json:"arg"`
 	Subtitle string `json:"subtitle"`
-	Title string `json:"title"`
+	Title    string `json:"title"`
+}
+
+type Flags struct {
+	Query       string
+	AccessToken string
 }
 
 func main() {
+	f := Flags{}
+	flag.StringVar(&f.Query, "query", "something", "something you want to search...")
+	flag.StringVar(&f.AccessToken, "token", "", "github access token")
+	flag.Parse()
+
+	exitCode := 0
+	if err := run(f); err != nil {
+		fmt.Printf("%v", err)
+		exitCode = -1
+	}
+	os.Exit(exitCode)
+}
+
+func run(f Flags) error {
 	ctx := context.Background()
-	//TODO: move github token into a cli flag
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: ""})
+	if len(f.AccessToken) == 0 {
+		return errors.New("access token can't be blank")
+	}
+
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: f.AccessToken})
 	tc := oauth2.NewClient(ctx, ts)
 
 	client := github.NewClient(tc)
 
 	opts := &github.SearchOptions{Sort: "created", Order: "desc"}
-	//TODO: read input query from cli flag
-	res, _, err := client.Search.Code(ctx, "docker repo:wayanjimmy/notebook", opts)
+
+	res, _, err := client.Search.Code(ctx, fmt.Sprintf("%s repo:wayanjimmy/notebook", f.Query), opts)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	items := []Item{}
 
 	for _, cr := range res.CodeResults {
+		item := Item{Arg: cr.GetHTMLURL(), Subtitle: cr.GetPath(), Title: cr.GetName()}
+		items = append(items, item)
+	}
+
+	res2, _, err := client.Search.Code(ctx, fmt.Sprintf("%s repo:wayanjimmy/zettelkasten", f.Query), opts)
+	if err != nil {
+		return err
+	}
+
+	for _, cr := range res2.CodeResults {
 		item := Item{Arg: cr.GetHTMLURL(), Subtitle: cr.GetPath(), Title: cr.GetName()}
 		items = append(items, item)
 	}
@@ -48,8 +82,10 @@ func main() {
 
 	m, err := json.Marshal(sr)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	fmt.Println(string(m))
+
+	return nil
 }
